913d541642e15d15d3171491cbff2d4c8f1f4a50
Samuel Miller <kamn.miller@gmail.com>
2013-01-24 23:27:47 -0700

    Fixed a problem related to not rotating around the correct star

--- StarBurst.js
+++ StarBurst.js
@@ -15,7 +15,8 @@ Copyright 2012 Samuel Miller
 */
 
 /**
-*
+*NOTES
+*http://mrdoob.github.com/three.js/examples/misc_camera_orbit.html
 */
 
 /*===INIT===*/
@@ -36,9 +37,9 @@ var StarBurst = function(JSONArr){
 	this.geometry = null;
 	this.material = null;
 	this.mesh = null;
-	this.mouse = {x:0,y:0};
-	this.cameraMovement = {x:0,y:0,z:(Math.PI/2)};
-	this.cameraPosDirection = {x:true,y:true,z:true};
+	this.mouse = {x: 0 ,y: 0};
+	this.cameraMovement = {x: 0, y: 0, z: (Math.PI/2)};
+	this.cameraPosDirection = {x: true, y: true, z: true};
 	this.planeOn = true;
 	this.planeWidth = 500;
 	this.planeHeight = 500;
@@ -63,6 +64,8 @@ var StarBurst = function(JSONArr){
 	this.mouse.x = 0;
 	this.mouse.y = 0;
 	this.distance = 400;
+
+	this.focusPoint = new THREE.Vector3(0,0,0);
 }
 
 /**
@@ -82,25 +85,26 @@ StarBurst.prototype.getStarTypeColorData = function(type){
 */
 StarBurst.prototype.keyPressed = function(e){
 
+	var charCode = e.charCode;
+
 	//Check to see if 's' was pressed to stop the rotation
-	if(e.keyCode == 115 || e.keyCode == 83){
+	if(charCode == 115 || charCode == 83){
 		this.offsetX = 0;
 		this.offsetY = 0;
 	}
 
 	//Checks '+' or '=' and zooms in
-	if(e.keyCode == 45 || e.keyCode == 95){
-		this.distance+=2;
+	if(charCode == 45 || charCode == 95){
+		this.distance += 2;
 	}
 
 	//Checks '-' or '_' and zooms out
-	if(e.keyCode == 61 || e.keyCode == 43){
-		this.distance-=2;
+	if(charCode == 61 || charCode == 43){
+		this.distance -= 2;
 	}
 
 	//For debugging
-	//console.log(e.keyCode);
-
+	//console.log(e);
 }
 /**
 * Will calculate the drag and check to see if the user has clicked on anything
@@ -133,6 +137,9 @@ StarBurst.prototype.onMouseDown = function(e){
 	//Get the intersected objects
 	var intersects = ray.intersectObjects( this.scene.children );
 
+
+	console.log(this.scene.position);
+
 	if ( intersects.length > 0 ) {
 
 		//Getting the first selected object
@@ -156,6 +163,17 @@ StarBurst.prototype.onMouseDown = function(e){
 			//TODO: Put this into a function
 			
 			this.updateStarInfo(this.JSONArr[parseInt(INTERSECTED.name)]);
+
+			//TODO: Redo the camera position
+			//TODO: Also do a tweet
+			//TODO: Make a function for this.
+			this.focusPoint = INTERSECTED.position;
+			//this.cameraMovement = {x: 1000, y: 1000, z: (Math.PI/2)};
+			//this.camera.position.x = Math.sin(this.cameraMovement.x) * this.distance;
+			//this.camera.position.y = Math.sin(this.cameraMovement.y) * this.distance;
+			//this.camera.position.z = Math.cos(this.cameraMovement.x) * this.distance;
+
+			console.log(INTERSECTED);
 		}
 	}
 	
@@ -179,9 +197,12 @@ StarBurst.prototype.onMouseMove = function(e){
 		var centerX = currMouseX - (window.innerWidth/2);
 		var centerY = currMouseY - (window.innerHeight/2);
 
+		//`rotationSpeed` should be lowered for greater sensitivy.
+		var rotationSpeed = 1000;
+
 		//TODO: Figure out how to keep it in the same spot
-		var offsetX = (currMouseX - this.mouse.x)/1000;
-		var offsetY = (currMouseY - this.mouse.y)/1000;
+		var offsetX = (currMouseX - this.mouse.x)/ rotationSpeed;
+		var offsetY = (currMouseY - this.mouse.y)/ rotationSpeed;
 		
 		this.cameraMovement.x += offsetX/20;
 		this.cameraMovement.y += offsetY/20;
@@ -239,7 +260,7 @@ StarBurst.prototype.init = function(){
 	starInfoDiv.style.position = "absolute";
 	starInfoDiv.style.top = "10px";
 	starInfoDiv.className = "info";
-	starInfoDiv.innerHTML = "<strong>Controls</strong><p>Click and drag to rotate</p><p>Click a star to it's name</p><p>Press 's' to stop the rotation</p><p> '-' and '+' to zoom in/out";
+	starInfoDiv.innerHTML = "<strong>Controls</strong><p>Click and drag to rotate</p><p>Click a star to see it's name</p><p>Press 's' to stop the rotation</p><p> '-' and '+' to zoom in/out";
 	container.appendChild(starInfoDiv);
 
 
@@ -342,7 +363,7 @@ StarBurst.prototype.init = function(){
     	tempGeo.vertices.push(starVertex);
 
     	//TODO:
-    	var size  = 10;
+    	var size = 10;
     	
     	//DEV: Figure out what the options are for ParticleBasciMaterial
     	//TODO: There should be a lot of Options here
@@ -414,18 +435,43 @@ StarBurst.prototype.init = function(){
     this.renderer = new THREE.WebGLRenderer({alpha:0});
     
     //TODO: These need to be options
-    this.renderer.setSize(window.innerWidth,window.innerHeight);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
     
     //TODO: option of speficiying a element to attach to...?
     //Attach to the body of the document
     container.appendChild(this.renderer.domElement);
     
+    console.log(this.scene);
+    console.log(this.camera);
+    console.log(THREE.RenderPass);
+    /*var renderModel = new THREE.RenderPass(this.scene, this.camera);
+    /*var renderModel = new THREE.RenderPass( this.scene, this.camera );
+	/*var effectBloom = new THREE.BloomPass( 1.3 );
+	var effectScreen = new THREE.ShaderPass( THREE.ShaderExtras[ "screen" ] );
+
+	effectFXAA = new THREE.ShaderPass( THREE.ShaderExtras[ "fxaa" ] );
+
+	var width = window.innerWidth || 2;
+	var height = window.innerHeight || 2;
+
+	effectFXAA.uniforms[ 'resolution' ].value.set( 1 / width, 1 / height );
+
+	effectScreen.renderToScreen = true;
+
+	composer = new THREE.EffectComposer( renderer );
+
+	composer.addPass( renderModel );
+	composer.addPass( effectFXAA );
+	composer.addPass( effectBloom );
+	composer.addPass( effectScreen );*/
+
+
     //TODO: Add as option
     //Attach listener(s)
 	document.addEventListener('mousedown',this.onMouseDown.bind(this),false);
 	document.addEventListener('mousemove',this.onMouseMove.bind(this),false);
 	document.addEventListener('mouseup',this.onMouseUp.bind(this),false);
-	document.addEventListener('keypress',this.keyPressed.bind(this),false);
+	document.addEventListener('keypress',this.keyPressed.bind(this), true);
 
 
 }	
@@ -466,19 +512,20 @@ StarBurst.prototype.render = function(){
 	
 	//TODO: Also have these be options
 	
-	this.camera.position.x = Math.sin(this.cameraMovement.x) * this.distance;
-	this.camera.position.y = Math.sin(this.cameraMovement.y) * this.distance
-	this.camera.position.z = Math.cos(this.cameraMovement.x) * this.distance
+	//TODO: Have an additional 
+	this.camera.position.x = this.focusPoint.x + Math.sin(this.cameraMovement.x) * this.distance;
+	this.camera.position.y = this.focusPoint.y + Math.sin(this.cameraMovement.y) * this.distance;
+	this.camera.position.z = this.focusPoint.z + Math.cos(this.cameraMovement.x) * this.distance;
 		
 	this.cameraMovement.x += this.offsetX/20;
-	this.cameraMovement.y +=  this.offsetY/20;
-	this.cameraMovement.z +=  this.offsetX/20;
+	this.cameraMovement.y += this.offsetY/20;
+	this.cameraMovement.z += this.offsetX/20;
 	
 	//Adjust camera
-	this.camera.lookAt(this.scene.position);
+	this.camera.lookAt(this.focusPoint);
 
 	//Render
-	this.renderer.render(this.scene,this.camera);
+	this.renderer.render(this.scene, this.camera);
 }
 
 
